e310f60cdac31d9277de0fe419225e789d6886df
s21120 <80425463+Keto1337@users.noreply.github.com>
2021-11-06 18:56:46 +0100

    final commit. Calculator done

---
+++ Task_4_5/Calculator_solution/Calculator/Client.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Client
+    {
+        public static void ClientCode(AbstractHandler handler)
+        {
+            foreach (var expression in new List<string> { "29-3", "2*10", "63/9", "18^3", "5+39"})
+            {
+                Console.WriteLine($"Client: Who can calculate {expression} ?");
+
+                var result = handler.Handle(expression);
+
+                if (result != null)
+                {
+                    Console.WriteLine($"\t{result}");
+                }
+                else
+                {
+                    Console.WriteLine($"\tNobody could solve it: {expression}");
+                }
+            }
+        }
+    }
+}
